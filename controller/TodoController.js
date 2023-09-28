44e93a30e3a8a5da753a91069f6a24c2f8465d96
JavDaGreat <98164907+JavDaGreat@users.noreply.github.com>
2023-09-28 21:58:34 +0200

    update

--- controller/TodoController.js
+++ controller/TodoController.js
@@ -6,7 +6,7 @@ const getAllTodo = async (req, res) => {
 
   const Todo = foundUser.tasks;
   console.log(Todo);
-  if (!Todo) return res.sendStatus(204);
+  if (!Todo || Todo.length === 0) return res.sendStatus(204);
   res.json(Todo);
 };
 
@@ -25,38 +25,39 @@ const createTodo = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
 const updateTodo = async (req, res) => {
-  const { id, newTodo, oldTodo } = req.body;
-  if (!newTask.title && !newTodo.description)
+  const { id, newTodo, todoId } = req.body;
+
+  if (!newTodo.title && !newTodo.description)
     return res.status(400).json({ message: "todo can not be empty" });
   try {
     const foundUser = await User.findById(id);
     const taskToUpdate = foundUser.tasks.find(
-      (task) =>
-        task.title === oldTodo.title && task.description === oldTodo.description
+      (task) => task._id.toString() === todoId
     );
-    if (!taskToUpdate) return res.sendStatus(404);
     console.log(taskToUpdate);
+
+    if (!taskToUpdate) return res.sendStatus(404);
+
     taskToUpdate.title = newTodo.title;
     taskToUpdate.description = newTodo.description;
 
     await foundUser.save();
 
-    res.sendStatus(201).json({ message: "success" });
+    res.status(201).json({ message: "success" });
   } catch (err) {
     res.sendStatus(500).json(err.message);
   }
 };
 
 const deleteTodo = async (req, res) => {
-  const { id, todo } = req.body;
-  if (!todo || !id)
-    return res.status(400).json({ message: "title can not be empty" });
+  const { id, todoId } = req.body;
+  if (!todoId || !id) return res.sendStatus(400);
   const foundUser = await User.findById(id);
   const taskToDelete = foundUser.tasks.findIndex(
-    (task) => task.title === todo.title && task.description === todo.description
+    (task) => task._id.toString() === todoId
   );
-
   console.log(taskToDelete);
   foundUser.tasks.splice(taskToDelete, 1);
 
